dac5c5d682c3ea181a2804e9a400f37d8e9eb605
Vrishab ML <vrishabm0@gmail.com>
2019-03-14 10:38:55 +0530

    Added models folder and refactoring

--- playground/mongodb-update.js
+++ playground/mongodb-update.js
@@ -10,7 +10,7 @@ const {MongoClient, ObjectID} = require('mongodb');
 MongoClient.connect('mongodb://localhost:27017/TodoApp', (err,db) => {
   if(err){
     return console.log('Unable to connect to Mongodb server');
-  }
+  }   
   console.log('Connected to Mongodb server');
 
   db.collection('Todo').findOneAndUpdate({
---
+++ server/models/user.js
@@ -0,0 +1,11 @@
+var mongoose = require('mongoose');
+var User = mongoose.model('User',{
+  email:{
+    type:String,
+    required:true,
+    trim:true,
+    minlength: 1
+  }
+});
+
+module.export = {User}
