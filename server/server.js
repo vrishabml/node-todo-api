b6a4b77c295c5ba860ec3850824dc8bfa35903d8
Vrishab ML <vrishabm0@gmail.com>
2019-03-24 03:14:53 +0530

    add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 
 const express = require('express');
+const _ =  require('lodash');
 const bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose.js');
 var {Todo} = require('./models/todo.js');
 var {User} = require('./models/user.js');
-var {ObjectID} = require('mongodb');
+
 
 var app = express();
 const port = process.env.PORT || 3000;
@@ -59,6 +61,31 @@ app.delete('/todos/:id', (req,res) => {
 }).catch((e) => {
   res.status(400).send();
 });
+});
+
+app.patch('/todos/:id', (req,res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body,['text','completed']);
+  if(!ObjectID.isValid(id))
+  {
+    return res.status(404).send('ID Invalid! Cannot delete');
+  }
+  if(_.isBoolean(body.completed) && body.completed){
+    body.completedAt = new Date().getTime();
+  }
+  else {
+    body.completed = false;
+    body.completedAt =null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new:true}).then((todo) => {
+    if(!todo){
+      return res.status(404).send();
+    }
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  })
 });
 
 app.listen(port,()=>{
